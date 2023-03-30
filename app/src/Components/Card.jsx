d74c2212cc13439a74c12354eacb2b241a82933e
Shantanu Sutar <shantanusutar17@gmail.com>
2023-03-30 23:18:28 +0530

    creating Active component
    where user gives the rating

--- app/src/Components/Card.jsx
+++ app/src/Components/Card.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
-
+import Active from "./Active";
 const Card = () => {
-  return <div className="card"></div>;
+  return (
+    <div className="card">
+      <Active />
+    </div>
+  );
 };
 
 export default Card;
